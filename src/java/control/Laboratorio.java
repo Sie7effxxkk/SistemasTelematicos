5a48a12b89206b5da81593ddda9bcb638f1d68a1
equipo <fercasis@unicauca.edu.co>
2020-09-30 18:36:05 -0500

    Clase Lobaratorio primera p}ueba

---
+++ src/java/control/Laboratorio.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+/**
+ *
+ * @author equipo
+ */
+public class Laboratorio {
+    public String holaMundo(){
+        return "{\"clave\":\"Hola mundo\"}";  //construccion Json
+    }
+}
